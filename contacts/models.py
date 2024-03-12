f7e27463c6597495e271c691a153e3571aa6be73
FawazAshrafAli <fawazashrafali@gmail.com>
2024-03-12 16:05:01 +0530

    Updated app 'contacts'.

--- contacts/models.py
+++ contacts/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from organizations.models import Company
 
 class Contact(models.Model):
     # name and occupation
     prefix = models.CharField(max_length=50, blank=False, null=False)
     first_name = models.CharField(max_length=100, blank=False, null=False)
-    last_name = models.CharField(max_length=100, blank=True, null=True)
-    organization = models.CharField(max_length=150, blank=False, null=False)
+    last_name = models.CharField(max_length=100, blank=True, null=True)    
+    organization = models.ForeignKey(Company, on_delete=models.PROTECT)
     title = models.CharField(max_length=150, blank=False, null=False)
 
     # contact details
@@ -23,33 +24,33 @@ class Contact(models.Model):
     twitter = models.CharField(max_length=100, blank=True, null=True)
 
     # address information
-    mailing_address = models.TextField()
-    mailing_city = models.CharField(max_length=50, blank=True, null=True)
-    mailing_state = models.CharField(max_length=50, blank=True, null=True)
-    mailing_postal_code = models.CharField(max_length=15, blank=True, null=True)
-    mailing_country = models.CharField(max_length=100, blank=True, null=True, default="India")
-    other_address = models.TextField()
+    mailing_address = models.TextField(blank=False, null=False)
+    mailing_city = models.CharField(max_length=50, blank=False, null=False)
+    mailing_state = models.CharField(max_length=50, blank=False, null=False)
+    mailing_postal_code = models.CharField(max_length=15, blank=False, null=False)
+    mailing_country = models.CharField(max_length=100, blank=False, null=False, default="India")
+    other_address = models.TextField(blank=True, null=True)
     other_city = models.CharField(max_length=50, blank=True, null=True)
     other_state = models.CharField(max_length=50, blank=True, null=True)
     other_postal_code = models.CharField(max_length=15, blank=True, null=True)
     other_country = models.CharField(max_length=100, blank=True, null=True, default="India")
 
     # dates to remember
-    due_date = models.DateField()
-    date_of_birth = models.DateField()
+    due_date = models.DateField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
 
     # description information
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
 
     # permission
     permission = models.CharField(max_length=100, blank=True, null=True)
 
     # tag information
-    tag_list = models.CharField(max_length=150)
+    tag_list = models.CharField(max_length=150, blank=True, null=True)
 
     # permissions
-    permissions = models.CharField(max_length=150)
+    permissions = models.CharField(max_length=150, blank=True, null=True)
 
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
--- contacts/views.py
+++ contacts/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse_lazy
 from django.contrib import messages
 from django.http  import Http404
 from django.contrib.auth.mixins import LoginRequiredMixin
+from organizations.models import Company
 
 class BaseContactView(LoginRequiredMixin):
     login_url = 'authentication:login'
@@ -12,27 +13,40 @@ class BaseContactView(LoginRequiredMixin):
 
 
 class CreateContactView(BaseContactView, CreateView):    
-    template_name = 'contacts/create_contact.html'
+    template_name = 'contacts/contacts.html'
     fields = [
         "prefix", "first_name", "last_name", "organization", "title", "email", "email_opted_out",
         "phone", "home_phone", "mobile_phone", "other_phone", "assistant_phone", "assistant_name",
         "fax", "linkedin", "facebook", "twitter", "mailing_address", "mailing_city", "mailing_state",
         "mailing_postal_code", "mailing_country", "other_address", "other_city", "other_state",
         "other_postal_code", "other_country", "due_date", "date_of_birth", "description", "permission",
-        "tag_list", "task_visibility"
+        "tag_list"
     ]
-    success_url = reverse_lazy('create_contact')    
+    success_url = reverse_lazy('contacts:list')    
 
     def form_valid(self, form):
         response = super().form_valid(form)
         messages.success(self.request, "New contact has been created!")
         return response
+    
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        for field, errors  in form.errors.items():
+            for error in errors:
+                print(f"Error on {field}: {error}")
+        return response
+
 
 
 class ListContactView(BaseContactView, ListView):
     template_name = 'contacts/contacts.html'
     queryset = Contact.objects.all()
-    context_object_name = "contacts"
+    context_object_name = "contacts"    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['organizations'] = Company.objects.all()
+        return context
 
 
 class DetailContactView(BaseContactView, DetailView):
