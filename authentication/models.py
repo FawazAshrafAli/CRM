e288800f7302ad0bbb48c2986f44d232e87923d4
FawazAshrafAli <fawazashrafali@gmail.com>
2024-03-19 14:42:35 +0530

    Changed the field name of model CrmUser from modified to updated

--- authentication/models.py
+++ authentication/models.py
@@ -49,7 +49,7 @@ class CrmUser(models.Model):
     experience = models.JSONField(blank=True, null=True)
 
     created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.name
