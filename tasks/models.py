c5944df405fa82392a78e00e868092f4525cd61f
FawazAshrafAli <fawazashrafali@gmail.com>
2024-03-19 16:28:19 +0530

    Replaced the datatype of assigned_to field from charfield to foreign key relating to CrmUser.

--- tasks/models.py
+++ tasks/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from authentication.models import CrmUser
 
 class Task(models.Model):
     # task details
     name = models.CharField(max_length=200, blank=False, null=False)
-    assigned_to = models.CharField(max_length=150, blank=True, null=True, default="Me")
+    assigned_to = models.ForeignKey(CrmUser, on_delete=models.PROTECT)
     category = models.CharField(max_length=150, blank=True, null=True, default="Email")
     due_date = models.CharField(max_length=50, blank=True, null=True)
 
@@ -13,7 +14,7 @@ class Task(models.Model):
     progress = models.PositiveIntegerField(default = 0)
     priority = models.CharField(max_length=150, blank=True, null=True, default="Low")
     status = models.CharField(max_length=150, default='Not Started')
-
+        
     # related to
     related_to = models.CharField(max_length=150, blank=True, null=True)
 
