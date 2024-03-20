1c3aca8b6ee94150198a9982950d662862ec8741
FawazAshrafAli <fawazashrafali@gmail.com>
2024-03-20 16:05:13 +0530

    Provided a basic outline structure model of app 'Activity'

--- activities/models.py
+++ activities/models.py
@@ -7,11 +7,39 @@ class Activity(models.Model):
     ending_date = models.DateField()
     ending_time = models.TimeField()
 
-    busy_or_free = models.CharField(max_length=150, blank=True, null=True, default="Busy")
+    availability = models.CharField(max_length=150, blank=True, null=True, default="Busy")
     notes = models.TextField()
 
     user_responsible = models.CharField(max_length=150, blank=False, null=False)
     deal_or_lead = models.CharField(max_length=150, blank=False, null=False)
     people = models.CharField(max_length=250, blank=False, null=False)
     organization = models.CharField(max_length=150, blank=False, null=False)
+
+    # meeting_follow_up_action = models.CharField(max_length=150, blank=True, null=True)
+
+    # # Meeting
+    # meeting_organizer = models.CharField(max_length=150, blank=True, null=True)
+    # meeting_title = models.CharField(max_length=150, blank=True, null=True)
+    # meeting_location = models.CharField(max_length=150, blank=True, null=True)
+    # meeting_participants = models.JSONField(blank=True, null=True, default=list)
+    # meeting_purpose = models.CharField(max_length=150, blank=True, null=True)
+    # meeting_notes = models.TextField(blank=True, null=True)    
+
+    # # Lunch/Meal
+    # meal_organizer = models.CharField(max_length=150, blank=True, null=True)
+    # mead_title = models.CharField(max_length=150, blank=True, null=True)    
+    # meal_location = models.CharField(max_length=150, blank=True, null=True)    
+    # meal_attendees = models.JSONField(blank=True, null=True, default=list)
+    # meal_menu = models.JSONField(blank=True, null=True, default=list)
+    # meal_purpose = models.CharField(max_length=150, blank=True, null=True)    
+    # meal_special_requirements = models.TextField(blank=True, null=True)
+
+    # # Flagged
+    # Title = models.CharField(max_length=150, blank=True, null=True)    
+    # flagged_datetime = models.DateTimeField(null=True, blank=True)
+    # reason_for_flagging = models.TextField(blank=True, null=True)
+    # flag_type = models.CharField(max_length=150, blank=True, null=True)    #e.g., priority level, status    
+    # flagged_user = models.CharField(max_length=150, blank=True, null=True)    
+    # due_date = models.DateField(null=True, blank=True)
+    # Status = models.CharField(max_length=150, blank=True, null=True)     # open/completed/dismissed
 
--- activities/views.py
+++ activities/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Activity
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 class BaseActivityView(LoginRequiredMixin):
     login_url = 'authentication:login'
     model = Activity
-
-class ActivityListView(BaseActivityView, ListView):
     template_name = "activities/activities.html"
+
+
+class ActivityListView(BaseActivityView, ListView):    
     queryset = Activity.objects.all()
+
+
+class ActivityCreateView(BaseActivityView, DetailView):
+    fields = "__all__"
