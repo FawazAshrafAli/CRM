e4e12f56777de22ff0ea8db866b9e0d050dbbd79
FawazAshrafAli <fawazashrafali@gmail.com>
2024-03-23 15:53:17 +0530

    Added url path for CloneContactView. Added url path for UpdateContactImageView.

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import ListContactView, CreateContactView, UpdateContactView, DeleteContactView, DetailContactView
+from .views import (ListContactView, CreateContactView, 
+                    UpdateContactView, DeleteContactView, 
+                    DetailContactView, UpdateContactImageView,
+                    CloneContactView)
 
 app_name = "contacts"
 
@@ -7,6 +10,8 @@ urlpatterns = [
     path('', ListContactView.as_view(),  name='list'),
     path('detail/<pk>', DetailContactView.as_view(), name='detail'),
     path('create/', CreateContactView.as_view(), name='create'),
+    path('clone/<pk>', CloneContactView.as_view(), name='clone'),
     path('update/<pk>', UpdateContactView.as_view(), name='update'),
+    path('update_image/<pk>', UpdateContactImageView.as_view(), name='update_image'),
     path('delete/<pk>', DeleteContactView.as_view(), name='delete'),
 ]
