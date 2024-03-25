463a85bfb6226c2146672baeb9c62e03b28b2201
FawazAshrafAli <fawazashrafali@gmail.com>
2024-03-25 16:42:13 +0530

    Import CrmUser and Lead. ListContView's queryset is modified so that archieved files with calue True will be avoided while rendering. CrmUser has been rendered to loop over it for the selection field in form. In DetailView before rendering to response record_owner has been overrided to send it properly. Organziation_id also has been serialized. Record_owner id and name has been serialized. UpdateView has been updated to print the errors and redirect when the form validation fails. UpdateContactOwner view has been created. LeadConvertion view to convert contact to lead has been created.

--- contacts/views.py
+++ contacts/views.py
@@ -7,13 +7,14 @@ from django.urls import reverse_lazy, reverse
 from django.contrib import messages
 from django.http  import Http404, HttpResponse, HttpResponseRedirect
 from django.contrib.auth.mixins import LoginRequiredMixin
-from organizations.models import Company
 from django.http import JsonResponse
+
 from organizations.models import Company
 from deals.models import Deal
 from projects.models import Project
-from django.utils import timezone
-from django.utils.timezone import timedelta
+from authentication.models import CrmUser
+from leads.models import Lead
+
 
 class BaseContactView(LoginRequiredMixin):
     login_url = 'authentication:login'
@@ -99,6 +100,7 @@ class CloneContactView(BaseContactView, CreateView):
 class ListContactView(BaseContactView, ListView):
     template_name = 'contacts/contacts.html'    
     context_object_name = "contacts"
+    queryset = Contact.objects.filter(archived = False)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -107,6 +109,7 @@ class ListContactView(BaseContactView, ListView):
             'related_contacts': Contact.objects.all(),
             'deals': Deal.objects.all(),
             'projects': Project.objects.all(),
+            'users': CrmUser.objects.all()
         })
         return context
 
@@ -123,7 +126,7 @@ class DetailContactView(BaseContactView, DetailView):
         # Auto dictionary creation using field name in a model and their respective value
         for field in contact._meta.fields:
             field_name = field.name
-            if field_name not in ("organization", "created", "updated", "image"):
+            if field_name not in ("organization", "created", "updated", "image", "record_owner"):
                 field_value = getattr(contact, field_name)
                 serialized_data[field_name] = field_value        
 
@@ -137,7 +140,10 @@ class DetailContactView(BaseContactView, DetailView):
             serialized_data["full_name"] = contact.first_name
 
         if contact.organization:
-            serialized_data["organization"] = contact.organization.name            
+            serialized_data.update({
+                "organization_id" : contact.organization.id,
+                "organization" : contact.organization.name
+                })            
 
         # Combining all fields of mailing addres to form full mailing address
         if contact.mailing_address and contact.mailing_city and contact.mailing_state and contact.mailing_postal_code and contact.mailing_country:
@@ -149,13 +155,11 @@ class DetailContactView(BaseContactView, DetailView):
             other_address = f"{contact.other_address}, {contact.other_city}, {contact.other_state}, {contact.other_postal_code}, {contact.other_country}."
             serialized_data['other_address'] = other_address
 
-        if contact.due_date:
-            due_date = contact.due_date.strftime("%d/%m/%Y")
-            serialized_data['due_date'] = due_date
-
-        if contact.date_of_birth:
-            date_of_birth = contact.date_of_birth.strftime("%d/%m/%Y")
-            serialized_data['date_of_birth'] = date_of_birth
+        if contact.record_owner:
+            serialized_data.update({
+                'record_owner': contact.record_owner.id,
+                'record_owner_name': contact.record_owner.name
+                })
 
         serialized_data.update({
             'created' : contact.created.strftime("%b %d, %Y"),
@@ -166,15 +170,22 @@ class DetailContactView(BaseContactView, DetailView):
 
 
 class UpdateContactView(BaseContactView, UpdateView):
+    model = Contact
     fields = "__all__"
     pk_url_kwarg = 'pk'
     context_object_name = "contact"
-    success_url = reverse_lazy('contact/contacts.html')
+    success_url = reverse_lazy('contacts:list')
     
     def form_valid(self, form):
         response = super().form_valid(form)
         messages.success(self.request, "Contact Updated.")
-        return response    
+        return response
+
+    def form_invalid(self, form):
+        for field, errors in form.errors.items():            
+            for error in errors:
+                print(f"Error in field {field}: {error}")
+        return redirect(reverse_lazy('authentication:error500'))
     
 
 class UpdateContactImageView(BaseContactView, UpdateView):
@@ -186,7 +197,17 @@ class UpdateContactImageView(BaseContactView, UpdateView):
         return super().form_valid(form)
 
 
-class DeleteContactView(LoginRequiredMixin, DeleteView):
+class UpdateContactOwner(BaseContactView, UpdateView):
+    modal = Contact
+    fields = ["record_owner"]
+    success_url = reverse_lazy('contacts:list')
+
+    def form_valid(self, form):
+        messages.success(self.request, "Record owner updated successfully.")
+        return super().form_valid(form)
+
+
+class DeleteContactView(BaseContactView, DeleteView):
     model = Contact
     pk_url_kwarg = 'pk'
     success_url = reverse_lazy('contacts:list')
@@ -204,3 +225,61 @@ class DeleteContactView(LoginRequiredMixin, DeleteView):
     def form_valid(self, form):
         messages.success(self.request, "Contact Deleted Successfully.")
         return super().form_valid(form)
+    
+
+class LeadConvertion(BaseContactView, CreateView):
+    modal = Lead
+    fields = "__all__"
+
+    def get_object(self, **kwargs):
+        try:
+            return get_object_or_404(Contact, pk = self.kwargs['pk'])            
+        except Http404:
+            return redirect(reverse_lazy('authentication:error404'))
+        
+    def post(self, *args, **kwargs):
+        self.object = self.get_object()
+        try:
+            get_object_or_404(Lead, organization = self.object.organization, email = self.object.email, phone = self.object.phone)
+            messages.warning(self.request, "Lead already exists.")
+            return redirect(reverse_lazy('contacts:list'))
+        except Http404:
+            pass
+
+        try:
+            Lead.objects.create(
+                prefix = self.object.prefix,
+                first_name = self.object.first_name,
+                last_name = self.object.last_name,
+                organization = self.object.organization,
+                title = self.object.title,                
+                lead_owner = self.object.record_owner,
+                email = self.object.email,
+                email_opted_out = self.object.email_opted_out,
+                phone = self.object.phone,
+                mobile_phone = self.object.mobile_phone,
+                fax = self.object.fax,                
+                mailing_address = self.object.mobile_phone,
+                mailing_city = self.object.mobile_phone,
+                mailing_state = self.object.mobile_phone,
+                mailing_postal_code = self.object.mobile_phone,
+                mailing_country = self.object.mobile_phone,
+                description = self.object.description,
+                tag_list =  self.object.tag_list
+            )
+            self.object.archived = True
+            self.object.save()
+            return redirect(self.get_success_url())
+        except Exception as e:
+            print(e)
+            return redirect(reverse_lazy('authentication:error500'))
+        
+    def form_valid(self, form):
+        messages.success(self.request, "Contact successfully converted to lead.")
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        for field, errors in form.errors.items():
+            for error in errors:
+                print(f"Error on {field}: {error}")
+        return redirect(reverse_lazy('authentication:error500'))
