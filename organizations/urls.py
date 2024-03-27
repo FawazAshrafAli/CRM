fd1dd24ad1edd211e96a67e9eff93ca8ac90bb3c
FawazAshrafAli <fawazashrafali@gmail.com>
2024-03-27 14:46:05 +0530

    Added url path for ChangeOrganizationImageView.

--- organizations/urls.py
+++ organizations/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views  import CreateOrganizationView, ListOrganizationView, DetailOrganizationView, UpdateOrganizationView, DeleteOrganizationView
+from .views  import (CreateOrganizationView, ListOrganizationView, 
+                     DetailOrganizationView, UpdateOrganizationView, 
+                     DeleteOrganizationView, ChangeOrganizationImageView)
 
 app_name = "organizations"
 
@@ -7,6 +9,7 @@ urlpatterns = [
     path('', ListOrganizationView.as_view(), name="list"),
     path('create/', CreateOrganizationView.as_view(), name="create"),
     path('detail/<pk>', DetailOrganizationView.as_view(), name="detail"),
-    path('update/<pk>', UpdateOrganizationView.as_view(), name="update"),
+    path('update/<pk>', UpdateOrganizationView.as_view(), name="update"),    
+    path('change_image/<pk>', ChangeOrganizationImageView.as_view(), name="change_image"),
     path('delete/<pk>', DeleteOrganizationView.as_view(), name="delete")
 ]
