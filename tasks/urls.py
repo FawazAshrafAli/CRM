1f9af5e1325deff892abdfb21f342be9263a037c
FawazAshrafAli <fawazashrafali@gmail.com>
2024-03-27 09:41:40 +0530

    Added url path for CompletedTaskListView.

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from .views import (TaskCreateView, CloneTaskView, TaskUpdateView, 
                     TaskListView, TaskDetailView, TaskDeleteView, 
                     TaskOwnerUpdateView, TaskCompletionView, 
-                    TaskCompletionAndCloningView)
+                    TaskCompletionAndCloningView, CompletedTaskListView)
 
 app_name = "tasks"
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='list'),
+    path('list_completed/', CompletedTaskListView.as_view(), name='list_completed'),
     path('update/<pk>', TaskUpdateView.as_view(), name='update'),
     path('update_task_owner/<pk>', TaskOwnerUpdateView.as_view(), name='update_task_owner'),
     path('complete/<pk>', TaskCompletionView.as_view(), name='complete'),
