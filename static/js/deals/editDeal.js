f79f43aefff1824a6b4b630cd47e48d6542b5f9c
FawazAshrafAli <fawazashrafali@gmail.com>
2024-04-04 14:20:22 +0530

    Updated editDeal.js to fetch more datas.

--- static/js/deals/editDeal.js
+++ static/js/deals/editDeal.js
@@ -3,15 +3,27 @@ function editDeal (dealId) {
         type: 'GET',
         url: '/deals/detail/' + dealId,
         dataType: 'json',
-        success: function (deal) {
+        success: function (deal) {              
             $('#edit-deal-form').prop('action', '/deals/update/'+ deal.id);
 
-            if (deal.name != null && deal.name != '') {
-                $('#name-value').prop('value', deal.name);
+            if (deal.prefix != null && deal.prefix != '') {
+                $('#prefix-value').prop('value', deal.prefix);
             } else {
-                $('#name-value').prop('value', '');
+                $('#prefix-value').prop('value', '');
+            };
+
+            if (deal.first_name != null && deal.first_name != '') {
+                $('#first-name-value').prop('value', deal.first_name);
+            } else {
+                $('#first-name-value').prop('value', '');
             };
             
+            if (deal.last_name != null && deal.last_name != '') {
+                $('#last-name-value').prop('value', deal.last_name);
+            } else {
+                $('#last-name-value').prop('value', '');
+            };
+
             var companySelect = document.getElementById('company-select');                        
             if (deal.company_id != null && deal.company_id != '') {
                 for (var i = 0; i < companySelect.options.length; i++) {
@@ -27,6 +39,59 @@ function editDeal (dealId) {
                 };
             };
 
+            if (deal.title != null && deal.title != '') {
+                $('#title-value').prop('value', deal.title);
+            } else {
+                $('#title-value').prop('value', '');
+            };
+
+            if (deal.email != null && deal.email != '') {
+                $('#email-value').prop('value', deal.email);
+            } else {
+                $('#email-value').prop('value', '');
+            };
+
+            if (deal.email_opted_out != null && deal.email_opted_out != '') {
+                $('#email-opted-out-value').prop('checked', true);
+            } else {
+                $('#email-opted-out-value').prop('checked', '');
+            };
+
+            if (deal.phone != null && deal.phone != '') {
+                $('#phone-value').prop('value', deal.phone);
+            } else {
+                $('#phone-value').prop('value', '');
+            };
+
+            if (deal.mobile_phone != null && deal.mobile_phone != '') {
+                $('#mobile-phone-value').prop('value', deal.mobile_phone);
+            } else {
+                $('#mobile-phone-value').prop('value', '');
+            };
+
+            if (deal.fax != null && deal.fax != '') {
+                $('#fax-value').prop('value', deal.fax);
+            } else {
+                $('#fax-value').prop('value', '');
+            };
+
+            if (deal.website != null && deal.website != '') {
+                $('#website-value').prop('value', deal.website);
+            } else {
+                $('#website-value').prop('value', '');
+            };
+
+            if (deal.industry != null && deal.industry != '') {
+                $('#industry-value').prop('value', deal.industry);
+            } else {
+                $('#industry-value').prop('value', '');
+            };
+
+            if (deal.number_of_employees != null && deal.number_of_employees != '') {
+                $('#number-of-employees-value').prop('value', deal.number_of_employees);
+            } else {
+                $('#number-of-employees-value').prop('value', '');
+            };
 
             var categorySelect = document.getElementById('category-select');
             if (deal.category != null && deal.category != '') {
@@ -111,7 +176,47 @@ function editDeal (dealId) {
                     bidTypeSelect.options[i].selected = false;
                 };
             };
+
+            if (deal.mailing_address != null && deal.mailing_address != '') {
+                $('#mailing-address-value').prop('value', deal.mailing_address);
+            } else {
+                $('#mailing-address-value').prop('value', '');
+            };
+
+            if (deal.mailing_city != null && deal.mailing_city != '') {
+                $('#mailing-city-value').prop('value', deal.mailing_city);
+            } else {
+                $('#mailing-city-value').prop('value', '');
+            };
+
+            if (deal.mailing_state != null && deal.mailing_state != '') {
+                $('#mailing-state-value').prop('value', deal.mailing_state);
+            } else {
+                $('#mailing-state-value').prop('value', '');
+            };
+
+            if (deal.mailing_postal_code != null && deal.mailing_postal_code != '') {
+                $('#mailing-postal-code-value').prop('value', deal.mailing_postal_code);
+            } else {
+                $('#mailing-postal-code-value').prop('value', '');
+            };
             
+            var mailingCountryValue = document.getElementById('mailing-country-value');
+
+            if (deal.mailing_country != null && deal.mailing_country != '') {
+                for (var i = 0; i < mailingCountryValue.options.lenght; i++) {
+                    if (mailingCountryValue.options[i].value == deal.mailing_country) {
+                        mailingCountryValue.options[i].selected = true;
+                    } else {
+                        mailingCountryValue.options[i].selected = false;
+                    };
+                };
+            } else {
+                for (var i = 0; i < mailingCountryValue.options.lenght; i++) {
+                    mailingCountryValue.options[i].selected = false;
+                };
+            };
+
             if (deal.description != null && deal.description != '') {
                 $('#description-value').prop('value', deal.description);
             } else {
