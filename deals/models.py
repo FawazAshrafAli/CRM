f6f870b430fe42a168f9071e9eadeabc4e9cfa32
FawazAshrafAli <fawazashrafali@gmail.com>
2024-04-04 14:14:59 +0530

    Added fields prefix, first_name, last_name, title, email, email_opted_out, phone, mobile_phone, fax, website, industry, number_of_employees, mailing_address, mailing_city, mailing_state, mailing_postal_code, mailing_country into model Deal.

--- deals/models.py
+++ deals/models.py
@@ -11,10 +11,23 @@ class PipelineStage(models.Model):
 class Deal(models.Model):
     image = models.ImageField(upload_to='deal_images/', blank=True, null=True)
 
-    name = models.CharField(max_length=150, blank=False, null=False)    
+    prefix = models.CharField(max_length=25, blank=True, null=True)
+    first_name = models.CharField(max_length=100, blank=False, null=False)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    # name = models.CharField(max_length=150, blank=False, null=False)    
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150, blank=False, null=False)
 
     # Additional Informations
+    email = models.EmailField(max_length=254, blank=False, null=False)
+    email_opted_out = models.BooleanField(default=False)
+    phone = models.CharField(max_length=20, blank=False, null=False)
+    mobile_phone = models.CharField(max_length=20, blank=True, null=True)
+    fax = models.CharField(max_length=20, blank=True, null=True)
+    website = models.CharField(max_length=150, blank=True, null=True)
+    industry = models.CharField(max_length=150, blank=True, null=True)
+    number_of_employees = models.CharField(max_length=50, blank=True, null=True)
+
     category = models.CharField(max_length=150, blank=True, null=True, default="Email")
     probability_of_winning = models.CharField(max_length=150, blank=True, null=True, default="Email")
     forecast_close_date = models.CharField(max_length=150, blank=True, null=True)
@@ -23,6 +36,13 @@ class Deal(models.Model):
     deal_value = models.CharField(max_length=100, blank=True, null=True)
     bid_amount = models.CharField(max_length=50, blank=True, null=True)
     bid_type = models.CharField(max_length=100, blank=True, null=True, default="Fixed Bid")
+
+    # Address Information
+    mailing_address = models.TextField(blank=False, null=False)
+    mailing_city = models.CharField(max_length=50, blank=False, null=False)
+    mailing_state = models.CharField(max_length=50, blank=False, null=False)
+    mailing_postal_code = models.CharField(max_length=15, blank=False, null=False)
+    mailing_country = models.CharField(max_length=100, blank=False, null=False)
 
     # Description Information
     description = models.TextField(blank=True, null=True)
